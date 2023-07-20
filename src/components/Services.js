358c77693b59d7a117b6a5b8f9360ead6ae84ed8
Muhammad Hamza <45312947+thehamzza@users.noreply.github.com>
2023-07-21 02:54:24 +0500

    Update Services.js

--- src/components/Services.js
+++ src/components/Services.js
@@ -87,7 +87,8 @@ export const Services = () => {
                     >
                       <img src={mobile} className='w-16 h-16 object-contain' />
                       <h5 className='text-white text-[18px] font-bold'>
-                        Data Science: CV and ML
+                        Data Science
+                        <h6>Machine Learning & Computer Vision</h6>
                       </h5>
                     </div>
                   </motion.div>
